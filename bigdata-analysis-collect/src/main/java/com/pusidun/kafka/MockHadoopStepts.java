2eaca833cf0c72a47c845af8b3a665442658a810
592470161@qq.com <Wxq11235813>
2020-07-23 22:12:50 +0800

    多job串行生成数据

---
+++ bigdata-analysis-collect/src/main/java/com/pusidun/kafka/MockHadoopStepts.java
@@ -0,0 +1,36 @@
+package com.pusidun.kafka;
+
+import com.pusidun.utils.InfoUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * 串行jobs数据
+ */
+public class MockHadoopStepts {
+
+    public static void main(String[] args) {
+        genFile();
+    }
+    public static void genFile(){
+        File file = new File("./stepts");
+        if(!file.exists()){
+            file.mkdir();
+        }
+
+        for (int j = 0; j < 4 ; j++) {
+            try {
+                String fileName = "./stepts/"+j+".txt";
+                FileOutputStream fos = new FileOutputStream(fileName,false);
+                for (int i = 1; i <= 10000 ; i++) {
+                    String msg =  InfoUtils.getSearchWords() +"\n";
+                    fos.write(msg.getBytes());
+                }
+                fos.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
