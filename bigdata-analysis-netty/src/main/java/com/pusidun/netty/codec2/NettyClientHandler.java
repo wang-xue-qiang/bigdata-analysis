8bca096a8a16b7a52069ae7deb473ff9bb311a3a
592470261@qq.com <Wxq11235813>
2020-12-22 23:22:29 +0800

    ProtoBuf学习

---
+++ bigdata-analysis-netty/src/main/java/com/pusidun/netty/codec2/NettyClientHandler.java
@@ -0,0 +1,48 @@
+package com.pusidun.netty.codec2;
+
+import com.pusidun.netty.codec.StudentPOJO;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.util.Random;
+
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    //当通道就绪就会触发该方法
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+
+        //随机发送一个Student 或者Worker
+        int random = new Random().nextInt(2);
+        MyDataInfo.MyMessage myMessage = null;
+        if (0 == random) {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.StudentType)
+                    .setStudent(MyDataInfo.Student.newBuilder().setId(5).setName("花和尚 鲁智深").build()).build();
+        } else {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.WorkerType)
+                    .setWorker(MyDataInfo.Worker.newBuilder().setAge(28).setName("王雪强").build()).build();
+        }
+
+        ctx.writeAndFlush(myMessage);
+    }
+
+    //当通道有读取事件时，会触发
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器的地址： " + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
